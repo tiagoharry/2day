35db7eff3704f406ad57730f2f6f4e2387a0d45b
Tiago Assis <tiagoassisandrade@hotmail.com>
2023-04-19 17:28:27 -0300

    inicio segunda secao mobile

---
+++ javaScript/main.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.menu-open')
+const menuDrop = document.querySelector('.menu-drop')
+
+menu.addEventListener('click', () => {
+  menuDrop.classList.toggle('hide')
+})
