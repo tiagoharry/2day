fa88733f10e927cde5f931b96e246fc515dc003d
Tiago Assis <tiagoassisandrade@hotmail.com>
2023-07-04 10:54:16 -0300

    english menu mobile

--- javaScript/englishText/menu.js
+++ javaScript/englishText/menu.js
@@ -1,10 +1,10 @@
 export function menu() {
-  const aboutUs = document.querySelector('.about');
-  const services = document.querySelector('.services');
-  const career = document.querySelector('.career');
-  const contact = document.querySelector('.contact');
-  const apointment = document.querySelector('.apointment');
-  const lang = document.querySelector('.nav_language');
+  const aboutUs = document.querySelector('.about.aboutMobile');
+  const services = document.querySelector('.services.sevicesMobile');
+  const career = document.querySelector('.career.careerMobile');
+  const contact = document.querySelector('.contact.contactMobile');
+  const apointment = document.querySelector('.apointment.apointmentMobile');
+  const lang = document.querySelector('.nav_language.mobile_language');
   
     aboutUs.textContent = "About us";
    
