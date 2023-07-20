ccdd1198b85a91e186bacf45747754c8244e25ee
Tiago Assis <tiagoassisandrade@hotmail.com>
2023-07-20 09:25:44 -0300

    changes in principal text width, photo mentoria, invert photos in oqueNosFazemos

---
+++ javaScript/language.js
@@ -0,0 +1,9 @@
+
+const ButtonLanguage = document.querySelector('.nav_language');
+const principalTitle = document.querySelector('.principal_title');
+
+ButtonLanguage.addEventListener('click', () => {
+  
+
+  principalTitle.textContent = "We work in the digital transformation of business/enterprises and startups.";
+});
